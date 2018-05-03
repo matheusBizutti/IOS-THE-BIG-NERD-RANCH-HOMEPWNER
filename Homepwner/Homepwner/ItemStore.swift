35986b1d67e8682be03d763db745ff7d20dc0f85
bizutti <matheus.bizutti@totvs.com.br>
2018-05-02 21:45:10 -0300

    HOMEPWNER - APP

---
+++ Homepwner/Homepwner/ItemStore.swift
@@ -0,0 +1,37 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by MATHEUS BIZUTTI on 26/04/18.
+//  Copyright © 2018 T1aluno04. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+
+}
---
+++ Homepwner/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by MATHEUS BIZUTTI on 27/04/18.
+//  Copyright © 2018 T1aluno04. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+}
